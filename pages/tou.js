3f1107bc8b2d953021593c5d4d85f4592611c324
Joshua Martinez <joshkmartinez@gmail.com>
2020-03-26 11:27:10 -0700

    add priv policy and tou

---
+++ pages/tou.js
@@ -0,0 +1,11 @@
+import { Heading, Text, Box } from 'rebass'
+import Layout from '../components/layout'
+
+const tou = () => (
+  <Layout>
+    <Box mx={4} p={1}>
+     
+    </Box>
+  </Layout>
+)
+export default tou
