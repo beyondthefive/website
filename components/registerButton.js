cb43650e92378ca351c75160c60b779c60f0710b
Joshua Martinez <joshkmartinez@gmail.com>
2020-03-28 23:20:51 -0700

    style change

--- components/registerButton.js
+++ components/registerButton.js
@@ -1,18 +1,20 @@
 import { Button, Text, Link } from 'rebass'
 
-const button = ({mx='auto'}) => {
-  return(<Button
-    m={4}
-    bg="#1c7ed6"
-    mx={mx}
-    sx={{ borderRadius: 10, boxShadow: '0 0 3px skyblue' }}
-  >
-    <Link variant="nav" href="/register">
-      <Text color="white" fontWeight="normal" fontSize={[3, 4, 5]}>
-        <b>Register now!</b>
-      </Text>
-    </Link>
-  </Button>)
+const button = ({ mx = 'auto' }) => {
+  return (
+    <Button
+      m={4}
+      bg="#1c7ed6"
+      mx={mx}
+      sx={{ borderRadius: 10, boxShadow: '0 0 1px black' }}
+    >
+      <Link variant="nav" href="/register">
+        <Text color="white" fontWeight="normal" fontSize={[3, 4, 5]}>
+          <b>Register now!</b>
+        </Text>
+      </Link>
+    </Button>
+  )
 }
 
 export default button
