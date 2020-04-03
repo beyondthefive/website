f3e9e373e90bfbf4e5338154be29807fc442c806
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-03 14:29:16 -0700

    lint

--- components/loadingIndicator.js
+++ components/loadingIndicator.js
@@ -1,7 +1,7 @@
-import { SpinnerCircular } from "spinners-react";
+import { SpinnerCircular } from 'spinners-react'
 
 const load = () => (
   <SpinnerCircular color="#1c7ed6" size={100} speed={123} thickness={200} />
-);
+)
 
-export default load;
+export default load
