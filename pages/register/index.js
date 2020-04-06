a0b5845b3c775545488cd376676079a6c469b6e7
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-05 17:20:23 -0700

    close regisration

--- pages/register/index.js
+++ pages/register/index.js
@@ -9,28 +9,22 @@ const register = () => {
   let onLoad = () => setLoaded(true)
   return (
     <Layout instructorAd={false}>
-      {!loaded ? (
+       {/*{!loaded ? (
         <Flex justifyContent="center" m={3}>
-          <LoadingIndicator/>
+          <LoadingIndicator />
         </Flex>
       ) : null}
-      <MobileView>
+     } <MobileView>
         <Flex>
           <Text my={4} mx={2} fontSize={[3, 4, 4]} textAlign="center">
             Please use a non-mobile device to register for Beyond The Five.
           </Text>
         </Flex>
-      </MobileView>
-      
-      <iframe
-        onLoad={() => onLoad()}
-        src="https://form.jotform.com/200454457448155"
-        width="100%"
-        height="100%"
-        marginHeight="0"
-        marginWidth="0"
-        frameBorder="0"
-      />
+      </MobileView>*/}
+
+      <Text my={5} fontSize={[3, 4, 4]} textAlign="center">
+        Registration is currently closed.
+      </Text>
     </Layout>
   )
 }
