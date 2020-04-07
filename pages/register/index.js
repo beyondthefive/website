33ac052d98eb136ffe64a9f5ad182d3d352b7d86
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-07 13:52:28 -0700

    open registration

--- pages/register/index.js
+++ pages/register/index.js
@@ -9,22 +9,22 @@ const register = () => {
   let onLoad = () => setLoaded(true)
   return (
     <Layout instructorAd={false}>
-       {/*{!loaded ? (
+      {!loaded ? (
         <Flex justifyContent="center" m={3}>
           <LoadingIndicator />
         </Flex>
       ) : null}
-     } <MobileView>
-        <Flex>
-          <Text my={4} mx={2} fontSize={[3, 4, 4]} textAlign="center">
-            Please use a non-mobile device to register for Beyond The Five.
-          </Text>
-        </Flex>
-      </MobileView>*/}
-
-      <Text my={5} fontSize={[3, 4, 4]} textAlign="center">
-        Registration is currently closed.
-      </Text>
+      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+      <iframe
+        onLoad={() => onLoad()}
+        className="airtable-embed airtable-dynamic-height"
+        src="https://airtable.com/embed/shrtLpsHjJtC3KIat?backgroundColor=gray"
+        frameBorder="0"
+        onmousewheel=""
+        width="100%"
+        height="4507"
+        style={{ background: 'transparent' }}
+      ></iframe>
     </Layout>
   )
 }
