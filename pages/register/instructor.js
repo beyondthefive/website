a4304885b05877cffec24ead94ec60843bfa1f8a
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-07 13:04:13 -0700

    close instructor apps

--- pages/register/instructor.js
+++ pages/register/instructor.js
@@ -9,29 +9,14 @@ const register = () => {
   let onLoad = () => setLoaded(true)
   return (
     <Layout instructorAd={false}>
-      {!loaded ? (
+      {/*{!loaded ? (
         <Flex justifyContent="center" m={3}>
           <LoadingIndicator />
         </Flex>
-      ) : null}
-      <MobileView>
-        <Flex>
-          <Text my={4} mx={2} fontSize={[3, 4, 5]} textAlign="center">
-            Please use a non-mobile device to apply for a position at Beyond The
-            Five.
-          </Text>
-        </Flex>
-      </MobileView>
-
-      <iframe
-        onLoad={() => onLoad()}
-        src="https://docs.google.com/forms/d/e/1FAIpQLSfeAjAUVn04UtO6Gm2gpk95Tbddh6UgsCCTTdR9svT0nB8oNQ/viewform?embedded=true"
-        width="100%"
-        height="1300"
-        marginHeight="0"
-        marginWidth="0"
-        frameBorder="0"
-      />
+      ) : null}*/}
+      <Text m={4} fontSize={3}>
+        Instructor applications are currently not open.
+      </Text>
     </Layout>
   )
 }
