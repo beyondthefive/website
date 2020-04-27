40e0faff5a38f1f4b9b678760d1f7adf34313ef8
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-27 11:17:02 -0700

    refactor

--- pages/discord.js
+++ pages/discord.js
@@ -1,7 +1,3 @@
-const a = () => (
-	<meta
-		httpEquiv="refresh"
-		content="0; url=https://discord.gg/WTbHyFY"
-	/>
+export default () => (
+	<meta httpEquiv="refresh" content="0; url=https://discord.gg/WTbHyFY"/>
 )
-export default a
--- pages/feedback.js
+++ pages/feedback.js
@@ -1,7 +1,7 @@
-const a = () => (
+export default () => (
 	<meta
 		httpEquiv="refresh"
 		content="0; url=https://forms.gle/R3YPSfiR1X2c1uvw9"
 	/>
 )
-export default a
+
