5d2bbfa3088a8bd1f5a2b67068a003e1af20f0cb
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-28 16:13:54 -0700

    lint

--- pages/5to5.js
+++ pages/5to5.js
@@ -1,9 +1,9 @@
-import Layout from "../components/layout";
+import Layout from '../components/layout'
 export default () => (
-  <Layout>
-    <meta
-      httpEquiv="refresh"
-      content="0; url=https://www.notion.so/beyondthefive/5-Hours-to-a-5-Beyond-The-Five-cd79fc8f499b45cca798ed1a1c942d7e"
-    />
-  </Layout>
-);
+	<Layout>
+		<meta
+			httpEquiv="refresh"
+			content="0; url=https://www.notion.so/beyondthefive/5-Hours-to-a-5-Beyond-The-Five-cd79fc8f499b45cca798ed1a1c942d7e"
+		/>
+	</Layout>
+)
