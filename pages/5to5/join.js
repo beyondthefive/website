835d0aff5c188d615d84f2c9893acae3c987f47f
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-30 16:23:00 -0700

    Add 5 to 5 zoom link redirect

---
+++ pages/5to5/join.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/layout'
+export default () => (
+	<Layout>
+		<meta
+			httpEquiv="refresh"
+			content="0; url=https://us02web.zoom.us/j/85083984198?pwd=azc3T0oyclRJd3NYbkw1Z2tJY2RwZz09"
+		/>
+	</Layout>
+)
