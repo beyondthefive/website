ba5091b62e83f75ae71d1a6e7cccd9c0d7560433
Joshua Martinez <joshkmartinez@gmail.com>
2020-04-30 16:23:05 -0700

    fix font

--- components/registerButton.js
+++ components/registerButton.js
@@ -9,7 +9,7 @@ const button = ({mx = 'auto'}) => {
 			sx={{borderRadius: 10, boxShadow: '0 0 1px black'}}
 		>
 			<Link variant="nav" href="/register">
-				<Text color="white" fontWeight="normal" fontSize={[3, 4, 5]}>
+				<Text sx={{fontFamily: 'IBM Plex Sans'}} color="white" fontWeight="normal" fontSize={[3, 4, 5]}>
 					<b>Register now!</b>
 				</Text>
 			</Link>
