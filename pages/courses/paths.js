abb324d59fd503176340b94054075a50b826e12d
Joshua Martinez <joshkmartinez@gmail.com>
2020-05-12 14:45:29 -0700

    add eng course path

--- pages/courses/paths.js
+++ pages/courses/paths.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, {Children} from 'react';
 import Layout from '../../components/layout';
 import {Box, Flex, Text} from 'rebass';
 import Math from '../../public/svgs/paths/math.svg';
 import Physics from '../../public/svgs/paths/physics.svg';
 import CS from '../../public/svgs/paths/cs.svg';
+import Eng from '../../public/svgs/paths/eng.svg';
 
-const Graphic = ({children}) => <Box my={3}>{children}</Box>;
+const paths = [
+	<Math width="100%" height="100%"/>,
+	<Physics width="100%" height="100%"/>,
+	<CS width="100%" height="100%"/>,
+	<Eng width="100%" height="100%"/>
+];
+
+const Graphic = ({children}) => <Box m={1}>{children}</Box>;
 
 const courses = () => {
 	return (
 		<Layout>
 			<Box width={[1, 4 / 5]} mx="auto" my={2} p={2}>
 				<Text fontSize={[3, 4, 5]}>Course Paths</Text>
-				<Flex flexDirection="column" justifyContent="center">
-					<Graphic>
-						<Math width="65%" height="65%"/>
-					</Graphic>
-					<Graphic>
-						<Physics width="65%" height="65%"/>
-					</Graphic>
-					<Graphic>
-						<CS width="65%" height="65%"/>
-					</Graphic>
-				</Flex>
+				<Box
+					sx={{
+						display: 'grid',
+						gridGap: 1,
+						gridTemplateColumns: ['repeat(1, 1fr)', 'repeat(2, 1fr)']
+					}}
+				>
+					{paths.map(c => (
+						<Graphic>{c}</Graphic>
+					))}
+				</Box>
 			</Box>
 		</Layout>
 	);
