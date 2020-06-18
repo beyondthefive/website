5617f78b103b5a01524743f77a52149e55bb209c
Joshua Martinez <joshkmartinez@gmail.com>
2020-06-17 21:37:44 -0700

    update testimonials

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,70 +1,64 @@
-import React, {useState} from 'react';
-import {Text, Box, Flex, Button} from 'rebass';
-import Swiper from 'react-id-swiper';
-import 'swiper/css/swiper.css';
+import React from "react";
+import { Text, Box, Flex, Button } from "rebass";
+import { Scrollbars } from "react-custom-scrollbars";
 
 const data = [
-	{
-		name: 'Emini',
-		quote:
-      'My experience with Beyond The Five is unlike anything I had ever imagined it to be. I\'ve always wanted to speed by school and to learn online, but many of the online learning platforms available required just too much. I did not want to keep creating accounts, to watch lecture videos that ended up being worthless, and to have questions that I would never get the answers to. But when I found Beyond the Five, I knew it was for me. The organization provides exceptional learning courses, along with fun and understandable instructors and TA\'s. I am eternally grateful that I found Beyond the Five. If it had not been for this organization, I probably would have given up on online instructional learning.'
-	},
-	{
-		name: 'David',
-		quote:
-      'I would like to thank Beyond the Five for everything that the organization has taught me. They transcend past the curricula tested on the AP Exam, and I feel that I am more than prepared to do well on the exams. The instructors have a deep understanding of the material tested on the exam, and they have done extraordinary things for the organization. I cannot thank the organization enough for everything they have taught me.'
-	},
-	{
-		name: 'Trenton',
-		quote:
-      'Beyond the Five has not only transformed my knowledge on the subjects I signed up for, it completely reshaped how I envision my future. The team at Beyond the Five is composed of some of the kindest, smartest people I\'ll ever meet, and I know that they truly care about both the program and the students. Whenever I have trouble with school, would like to discuss my high school plans, or just want to talk to some friends, I love being able to consult with the people at Beyond the Five. Joining Beyond the Five has honestly turned me onto a much higher path in life, and I strongly recommend all ambitious, driven students join.'
-	}
+  {
+    name: "Salah",
+    quote:
+      "Beyond the five is not only one of the best programs for students who are hoping to expand their vast knowledge, but it also offers an inclusive community for students. I love reaching out and having conversations with students from all around the world who are all eager to learn and challenge themselves to their limits.",
+  },
+  {
+    name: "Paris",
+    quote:
+      "I believe Beyond The Five is a wonderful group of people. The fact that they’re providing free education to further one's knowledge and ability to pass the class and especially exams is amazing. My favorite part about Beyond The Five is the wide selection of classes they offer. The instructors at Beyond the Five are super nice as well and are available whenever you have questions. I am glad to be a part of such a wonderful community!",
+  },
+  {
+    name: "Adam",
+    quote:
+      "As someone who was completely unfamiliar with Beyond the Five before being suddenly thrust into the server, BT5's community was ridiculously welcoming and easy to talk with. From reading out Andy's messages in a falsetto voice to doing an impression of Dr. Seuss' Cat in the Hat, I've had a ridiculous amount of fun and I am looking forward to pushing myself to academic success with the BT5 team by my side.",
+  },
+  {
+    name: "Helen",
+    quote:
+      "BT5 is a wonderful community because they offer a wide varieties of classes, from AP to IGCSE prep. My favorite part about BT5 is that you're not the only one who is taking these classes. There will be other students for you to interact with, whether it's asking questions about the class or just having fun in general. The instructors are online and ready to support you if you are confused about something.",
+  },
 ];
 
-const Item = ({name, contents}) => (
-	<Box
-		bg="whitesmoke"
-		sx={{borderRadius: 5, boxShadow: '0 0 1px grey'}}
-		p={[2, 3, 4]}
-	>
-		<Text fontSize={[2, 3, 4]}>{contents}</Text>
-		<Text fontSize={[2, 3, 4]} m={2} textAlign="right">
-			- {name}
-		</Text>
-	</Box>
-);
-
 const Testimonials = () => {
-	const [swiper, setSwiper] = useState(null);
+  return (
+    <Box
+      width={1}
+      whiteSpace="nowrap"
+      sx={{ overflowX: "auto" }}
+      p={[1, 2, 3]}
+      sx={{
+        display: "grid",
+        gridGap: 1,
+        gridTemplateColumns: ["repeat(1, 1fr)", "repeat(2, 1fr)"],
+      }}
+    >
+      {data.map((t) => (
+        <Box
+          bg="white"
+          key={t.name}
+          px={4}
+          //py={1}
 
-	return (
-		<Box width={1} m={[1, 2]} p={[2, 4]}>
-			<Text fontSize={[3, 4, 5]} mb={[2, 3, 4]} textAlign={['center', 'left']}>
-				What our students have to say:
-			</Text>
-			<Flex>
-				<Swiper
-					{...{
-						navigation: {
-							nextEl: '.swiper-button-next',
-							prevEl: '.swiper-button-prev'
-						},
-						pagination: {
-							el: '.swiper-pagination',
-							clickable: true
-						}
-					}}
-					getSwiper={setSwiper}
-				>
-					{data.map(t => (
-						<Box key={t.name} px={5} py={1}>
-							<Item name={t.name} contents={t.quote}/>
-						</Box>
-					))}
-				</Swiper>
-			</Flex>
-		</Box>
-	);
+          sx={{
+            borderRadius: 5,
+            boxShadow: "-1px 1px 1px rgba(0, 0, 0, 0.25)",
+          }}
+          p={[1, 2, 3]}
+          m={[1, 2]}
+          fontSize={[1, 2, 3]}
+        >
+          <Text>"{t.quote}"</Text>
+          <Text textAlign="right">- {t.name}</Text>
+        </Box>
+      ))}
+    </Box>
+  );
 };
 
 export default Testimonials;
