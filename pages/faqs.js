43caf0b3892dcae0aaec7226ad0eb39ffed163d6
Joshua Martinez <joshkmartinez@gmail.com>
2020-07-18 15:55:54 -0700

    add faq search

--- pages/faqs.js
+++ pages/faqs.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import {Box, Flex, Text, Button} from 'rebass';
-import {FaArrowDown, FaArrowUp} from 'react-icons/fa';
+import {Input} from '@rebass/forms';
+import {FaChevronDown, FaChevronUp} from 'react-icons/fa';
 import Layout from '../components/layout';
 import LoadingIndicator from '../components/loadingIndicator';
 import fetchData from '../lib/faqs';
 
 export default () => {
 	const [data, setData] = useState([]);
+	const [query, setQuery] = useState('');
 	useEffect(() => {
 		const d = async () => {
 			// SetAdministrativeData(await fetchData());
@@ -16,12 +18,40 @@ export default () => {
 		d();
 	}, []);
 
+	const updateFilter = e => {
+		console.log('New value: ' + e.target.value);
+		setQuery(e.target.value);
+	};
+
+	const searchFilter = i => {
+		if (
+			i.Question.toLowerCase().includes(query) ||
+      i.Answer.toLowerCase().includes(query)
+		) {
+			return i;
+		}
+	};
+
 	return (
 		<Layout>
 			<Box width={[1, 4 / 5]} mx="auto" my={3} p={3}>
-				<Text fontSize={[3, 4, 5]} mb={2}>
-					Frequently Asked Questions
-				</Text>
+				<Flex
+					justifyContent="space-between"
+					alignItems="center"
+					flexDirection={['column', 'row']}
+					my={[3, 2, 1]}
+				>
+					<Text fontSize={[3, 4, 5]} mb={2} textAlign={['center', 'left']}>
+						Frequently Asked Questions
+					</Text>
+					<Box width={[1, 1 / 2]}>
+						<Input
+							placeholder="Search"
+							value={query}
+							onChange={e => updateFilter(e)}
+						/>
+					</Box>
+				</Flex>
 				<Box
 					sx={{
 						display: 'grid',
@@ -32,9 +62,19 @@ export default () => {
 					{data.length === 0 ? (
 						<LoadingIndicator/>
 					) : (
-						data.map(i => <Card question={i.Question} answer={i.Answer}/>)
+						data
+							.filter(i => searchFilter(i))
+							.map(i => <Card question={i.Question} answer={i.Answer}/>)
 					)}
 				</Box>
+				<Text textAlign="right">
+					Displaying {data.filter(i => searchFilter(i)).length} of{' '}
+					{data.length} FAQs
+				</Text>
+				<Text mt={3} fontSize={[2, 3, 4]} textAlign="center">
+					Still have questions? Shoot an email to{' '}
+					<a href="mailto:team@beyondthefive.org">team@beyondthefive.org</a>
+				</Text>
 			</Box>
 		</Layout>
 	);
@@ -51,28 +91,21 @@ const Card = ({question, answer}) => {
 				border: '1px solid grey',
 				borderRadius: 5
 			}}
+			fontSize={[1, 2, 3]}
+			onClick={() => setOpen(!open)}
 		>
-			<Text fontSize={[1, 2, 3]}>
-				<Text mb={2} mx={2}>
-					<Text color="darkred">{question}</Text>
-					<Flex alignItems="center" justifyContent="space-between">
-						<Button p={0} variant="blank" onClick={() => setOpen(!open)}>
-							See Answer{' '}
-							{open ? (
-								<FaArrowUp size="0.75em"/>
-							) : (
-								<FaArrowDown size="0.75em"/>
-							)}
-						</Button>
-					</Flex>
-				</Text>
+			<Button sx={{fontFamily: 'IBM Plex Sans'}} p={0} width={1} variant="blank">
+				<Flex mb={2} mx={2} justifyContent="space-between" color="bt5blue">
+					<Text textAlign="left">{question}</Text>
 
-				{open ? (
-					<Box sx={{borderTop: '1px solid grey'}} p={3}>
-						<Text>{answer}</Text>
-					</Box>
-				) : null}
-			</Text>
+					{open ? <FaChevronUp size="1.2em"/> : <FaChevronDown size="1.2em"/>}
+				</Flex>
+			</Button>
+			{open ? (
+				<Box sx={{borderTop: '1px solid grey'}} p={3}>
+					<Text textAlign="left">{answer}</Text>
+				</Box>
+			) : null}
 		</Box>
 	);
 };
