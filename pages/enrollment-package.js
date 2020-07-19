8920fcb2c4ca8c8fb935ddeda12bd266c4755105
Joshua Martinez <joshkmartinez@gmail.com>
2020-07-19 10:46:43 -0700

    add enrollment package

---
+++ pages/enrollment-package.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import Layout from '../components/layout';
+import {Flex} from 'rebass';
+import LoadingIndicator from '../components/loadingIndicator';
+
+const register = () => {
+	const [loaded, setLoaded] = useState(false);
+	const onLoad = () => setLoaded(true);
+	return (
+		<Layout noFooter instructorAd={false}>
+			{!loaded ? (
+				<Flex justifyContent="center">
+					<LoadingIndicator/>
+				</Flex>
+			) : null}
+
+			<iframe
+				src="https://drive.google.com/file/d/1ziOucFfbeVVbv1dXFrphU0bTGbqxZ8xm/preview"
+				frameBorder="0"
+				width="100%"
+				position="absolute"
+				height="2100"
+				style={{background: 'transparent'}}
+				onLoad={() => onLoad()}
+			/>
+		</Layout>
+	);
+};
+
+export default register;
