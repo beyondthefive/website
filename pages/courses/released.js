3f6469185f580efbee487d59a7f523cfe660af3c
Joshua Martinez <joshkmartinez@gmail.com>
2020-07-27 12:11:30 -0700

    create released page

---
+++ pages/courses/released.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import Layout from '../../components/layout';
+import {Flex, Text, Box} from 'rebass';
+import LoadingIndicator from '../../components/loadingIndicator';
+
+export default () => {
+	const [loaded, setLoaded] = useState(false);
+	const onLoad = () => setLoaded(true);
+	return (
+		<Layout instructorAd={false}>
+			{!loaded ? (
+				<Flex justifyContent="center">
+					<LoadingIndicator/>
+				</Flex>
+			) : null}
+			<Text fontSize={[2, 3, 4]} mt={2} textAlign="center">
+				Released Courses
+			</Text>
+			<Box height="100%">
+				<iframe
+					className="airtable-embed"
+					src="https://airtable.com/embed/shrs5PQox4r1xmSyc?backgroundColor=gray&viewControls=on"
+					onmousewheel=""
+					width="100%"
+					height="100%"
+					frameBorder="0"
+					position="absolute"
+					style={{
+						background: 'transparent',
+						position: 'absolute'
+					}}
+					onLoad={() => onLoad()}
+				/>
+			</Box>
+		</Layout>
+	);
+};
