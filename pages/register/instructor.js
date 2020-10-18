6e986a3eb82f57d4d63645c49dcc31144a697197
Joshua Martinez <joshkmartinez@gmail.com>
2020-10-17 22:35:51 -0700

    add captcha to staff app

--- pages/register/instructor.js
+++ pages/register/instructor.js
@@ -1,29 +1,44 @@
-import React, {useState} from 'react';
-import Layout from '../../components/layout';
-import {Flex} from 'rebass';
-import LoadingIndicator from '../../components/loadingIndicator';
+import React, { useState } from "react";
+import Layout from "../../components/layout";
+import { Flex, Box } from "rebass";
+import LoadingIndicator from "../../components/loadingIndicator";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 
-export default () => {
-	const [loaded, setLoaded] = useState(false);
-	const onLoad = () => setLoaded(true);
-	return (
-		<Layout instructorAd={false}>
-			{!loaded ? (
-				<Flex justifyContent="center">
-					<LoadingIndicator/>
-				</Flex>
-			) : null}
-			<script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"/>
-			<iframe
-				className="airtable-embed airtable-dynamic-height"
-				src="https://airtable.com/embed/shr0g45zTMwkHGs7V?backgroundColor=gray"
-				frameBorder="0"
-				width="100%"
-				height="2445"
-				style={{background: 'transparent'}}
-				onLoad={() => onLoad()}
-			 />
-		</Layout>
-	);
+const register = () => {
+  const [captchaFilled, setCaptchaFilled] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const onLoad = () => setLoaded(true);
+  return (
+    <Layout instructorAd={false}>
+      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js" />
+      {!captchaFilled ? (
+        <Flex alignItems="center" justifyContent="center" m={[1, 2, 3]}>
+          <HCaptcha
+            sitekey="0e33b826-3740-4732-bbc5-98e27fba41d4"
+            onVerify={(token) => setCaptchaFilled(true)}
+          />
+        </Flex>
+      ) : (
+        <Box>
+          {!loaded ? (
+            <Flex justifyContent="center">
+              <LoadingIndicator />
+            </Flex>
+          ) : null}
+
+          <iframe
+            className="airtable-embed airtable-dynamic-height"
+            src="https://airtable.com/embed/shr0g45zTMwkHGs7V?backgroundColor=gray"
+            frameBorder="0"
+            width="100%"
+            height="2445"
+            style={{ background: "transparent" }}
+            onLoad={() => onLoad()}
+          />
+        </Box>
+      )}
+    </Layout>
+  );
 };
 
+export default register;
