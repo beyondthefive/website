7ccf8f3c465cfbaf6aecabc1460ca897c8421df5
Joshua Martinez <joshkmartinez@gmail.com>
2021-01-13 14:39:30 -0800

    lint

--- functions/stats.js
+++ functions/stats.js
@@ -1,16 +1,16 @@
-const moment = require('moment');
-const axios = require('axios');
+const moment = require('moment')
+const axios = require('axios')
 
 exports.handler = function (event, context, callback) {
 	axios
 		.get('https://beyondthefive.org/.netlify/functions/courses')
-		.then(response => {
+		.then((response) => {
 			const categories = {
 				AdvancedPlacement: 0,
 				CollegeLevel: 0,
 				CollegeTestPreparation: 0,
 				Miscellaneous: 0
-			};
+			}
 			const subjects = {
 				English: 0,
 				Biology: 0,
@@ -23,73 +23,73 @@ exports.handler = function (event, context, callback) {
 				CollegeTestPrep: 0,
 				Languages: 0,
 				Physics: 0
-			};
-			let out = {totalCourses: response.data.length};
-			response.data.map(c => {
+			}
+			let out = {totalCourses: response.data.length}
+			response.data.map((c) => {
 				if (c.Category == 'Advanced Placement') {
-					categories.AdvancedPlacement++;
+					categories.AdvancedPlacement++
 				} else if (c.Category == 'College-Level') {
-					categories.CollegeLevel++;
+					categories.CollegeLevel++
 				} else if (c.Category == 'College & Test Preparation') {
-					categories.CollegeTestPreparation++;
+					categories.CollegeTestPreparation++
 				} else if (c.Category == 'Miscellaneous') {
-					categories.Miscellaneous++;
+					categories.Miscellaneous++
 				}
 
 				if (c.Subject.includes('English')) {
-					subjects.English++;
+					subjects.English++
 				}
 
 				if (c.Subject.includes('Biology')) {
-					subjects.Biology++;
+					subjects.Biology++
 				}
 
 				if (c.Subject.includes('Chemistry')) {
-					subjects.Chemistry++;
+					subjects.Chemistry++
 				}
 
 				if (c.Subject.includes('Math')) {
-					subjects.Math++;
+					subjects.Math++
 				}
 
 				if (c.Subject.includes('History')) {
-					subjects.History++;
+					subjects.History++
 				}
 
 				if (c.Subject.includes('Social Sciences')) {
-					subjects.SocialSciences++;
+					subjects.SocialSciences++
 				}
 
 				if (c.Subject.includes('Computer Science')) {
-					subjects.ComputerScience++;
+					subjects.ComputerScience++
 				}
 
 				if (c.Subject.includes('Misc.')) {
-					subjects.Misc++;
+					subjects.Misc++
 				}
 
 				if (c.Subject.includes('College & Test Prep.')) {
-					subjects.CollegeTestPrep++;
+					subjects.CollegeTestPrep++
 				}
 
 				if (c.Subject.includes('Languages')) {
-					subjects.Languages++;
+					subjects.Languages++
 				}
 
 				if (c.Subject.includes('Physics')) {
-					subjects.Physics++;
+					subjects.Physics++
 				}
-			});
-			out = {...out, subjects, categories};
+			})
+			out = {...out, subjects, categories}
 			callback(null, {
 				statusCode: 200,
 				body: JSON.stringify(out)
-			});
+			})
 		})
-		.catch(error => {
+		.catch((error) => {
 			callback(null, {
 				statusCode: 200,
 				body: error
-			});
-		});
-};
+			})
+		})
+}
