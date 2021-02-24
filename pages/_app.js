7dcd3378440e4f545f361e1d8e30bb2f4bfbecb2
Joshua Martinez <joshkmartinez@gmail.com>
2021-02-24 13:47:00 -0800

    add og image

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import App from 'next/app'
-import Head from 'next/head'
+import App from 'next/app.js'
+import Head from 'next/head.js'
 import React from 'react'
 import {useAnalytics} from '@happykit/analytics'
 import {ThemeProvider} from 'theme-ui'
-import theme from '../lib/theme'
+import theme from '../lib/theme.js'
 
 const MyApp = ({Component, pageProps}) => {
 	useAnalytics({publicKey: 'pk_live_a5457a9a2d'})
@@ -17,6 +17,8 @@ const MyApp = ({Component, pageProps}) => {
 					name="description"
 					content="Beyond The Five is an online platform and community that provides free instruction in AP®️, SAT/ACT, and college level classes. 100% of our students pass their AP®️ exams. Registration for the 2020-21 school year is now open!"
 				/>
+				<meta property="og:image" content="/logo.png"/>
+
 				<link rel="icon" href="/favicon.ico" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap"
